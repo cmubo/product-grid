934b74e1ae6c1e1c2cd1f577f6dc89d50d8d96d8
Callum Mubery <callum@bmkdigital.co.uk>
2019-02-21 21:44:20 +0000

    alot, started with vue store, needs more configuring, build the start of the configurator, more styles

---
+++ layouts/partials/navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="container header-inner-container">
+      <nav>
+        <ul class="list-reset">
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/styles/style1">Style 1</nuxt-link></li>
+          <li><nuxt-link to="/styles/style2">Style 2</nuxt-link></li>
+        </ul>
+      </nav>
+      <div class="config-controls">
+        <a href="#">Hide</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
---
+++ pages/styles/style1.vue
@@ -0,0 +1,104 @@
+<template>
+  <section class="site-container">
+
+    <div class="colour-configurator">
+      <div class="config-container">
+        <div class="config-row">
+            <label>Product Background</label>
+            <input v-model="colours.productBackground" placeholder="#ffffff"></input>
+        </div>
+        <div class="config-row">
+          <label>Title</label>
+          <input v-model="colours.title" placeholder="#ffffff"></input>
+        </div>
+        <div class="config-row">
+          <label>Price</label>
+          <input v-model="colours.price" placeholder="#ffffff"></input>
+        </div>
+      </div>
+    </div>
+		<!-- TODO: Create a list view class which changes layout of products to full width -->
+		<div class="cm-products cm-grid-view cm-style-1">
+			<div class="cm-product-cell" v-for="item in productsList">
+				<div class="cm-product-inner" :style="{backgroundColor: colours.productBackground}">
+					<!-- TODO: Out of stock -->
+					<!-- TODO: On sale -->
+					<!-- TODO: Space for star rating/other snippet -->
+					<!-- TODO: Add To cart/Quick Buy -->
+					<div class="cm-product-img" :style="{ backgroundImage: 'url(' + item.images[0].src + ')' }">
+						<!-- TODO: Create background img layout -->
+						<a href="#">
+							<img :src="item.images[0].src">
+						</a>
+					</div>
+					<div class="cm-product-title">
+						<a href=""><div class="cm-title" :style="{color: colours.title}">{{ item.title }}</div></a>
+					</div>
+					<div class="cm-product-price">
+						<div class="cm-price" :style="{color: colours.price}">{{ item.variants[0].price}}</div>
+					</div>
+
+          <div class="cm-product-price price-with-sale" v-if="item.variants[0].compare_at_price">
+						<div class="cm-price" :style="{color: colours.price}">{{ item.variants[0].price}}</div>
+            <div class="compare-at-price" v-if="item.variants[0].compare_at_price" :style="{color: colours.salePrice}">{{ item.variants[0].compare_at_price }}</div>
+					</div>
+
+					<div class="cm-links">
+						<a class="cm-cart-link" :style="{backgroundColor: colours.addToCart}">Add To Cart</a>
+						<a class="cm-view-link">View Product</a>
+					</div>
+				</div>
+			</div>
+		</div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+import { mapState } from 'vuex';
+
+export default {
+  components: {
+
+  },
+  data: function(){
+      return {
+          shopifyProductResponse: null,
+          AllProductsJson: null,
+          productsList: null,
+          productsShown: false,
+          colours: {
+            pageBackground: "#000000",
+            productBackground: "#ffffff",
+            title: "#000000",
+            price: "#000000",
+            salePrice: "#ff0000",
+            addToCart: "#00ff00",
+          }
+      }
+
+  },
+  mounted: function(){
+      this.getAllProducts();
+  },
+  methods: {
+    //TODO: Move these products into the vue store so that product images can be changed
+      getAllProducts() {
+          let self = this
+          axios.get('/shopify/products')
+              .then(function (response) {
+                  self.shopifyProductResponse = response.data.body;
+                  self.productsList = JSON.parse(response.data.body).products;
+                  self.productsShown = true;
+
+                  console.log(response.data.body);
+              })
+              .catch(function (error) {
+                  console.log(error);
+              });
+      },
+
+  }
+}
+
+</script>
