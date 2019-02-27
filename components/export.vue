2d85efe353de09054baf1168b47fcc963c8c3ff3
Callum Mubery <callum@bmkdigital.co.uk>
2019-02-27 20:11:39 +0000

    Done some changes, Must be a better way of doing this, going to think about it

--- components/export.vue
+++ components/export.vue
@@ -4,8 +4,74 @@
     <div class="export-code-container">
       <div class="top-bar"><span>CSS</span></div>
       <code>
+        <div class="class-block">
+          <div>
+            .cm-product-cell, .cm-product-cell * {
+              <div><span>&nbsp;&nbsp;</span>box-sizing: border-box;</div>
+            }
+          </div>
+          <div>
+            .cm-product-cell:hover .cm-product-inner {
+              <div><span>&nbsp;&nbsp;</span>box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);</div>
+            }
+          </div>
+        </div>
 
-        <div v-if="product.background">{{product.background}}</div>
+        <div class="class-block">
+          <div>
+            .cm-product-cell {
+              <div><span>&nbsp;&nbsp;</span>display: flex;</div>
+              <div class="reactive">
+                <span>&nbsp;&nbsp;</span>padding: <span v-if="cell.padding">{{cell.padding}}</span><span v-else>15px</span>;
+              </div>
+            }
+          </div>
+          <div>
+            .cm-product-cell:hover .cm-product-inner {
+              <div><span>&nbsp;&nbsp;</span>box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);</div>
+            }
+          </div>
+        </div>
+        <div class="class-block">
+          .cm-product-inner {
+            <div><span>&nbsp;&nbsp;</span>position: relative;</div>
+            <div><span>&nbsp;&nbsp;</span>width: 100%;</div>
+            <div><span>&nbsp;&nbsp;</span>display: flex;</div>
+            <div><span>&nbsp;&nbsp;</span>flex-direction: column;</div>
+            <div><span>&nbsp;&nbsp;</span>padding-bottom: 70px;</div>
+            <div class="reactive">
+              <div v-if="product.paddingX">
+                <div><span>&nbsp;&nbsp;</span>padding-left: {{product.paddingX}};</div>
+                <div><span>&nbsp;&nbsp;</span>padding-right: {{product.paddingX}};</div>
+              </div>
+              <div v-else>
+                <div><span>&nbsp;&nbsp;</span>padding-left: 10px;</div>
+                <div><span>&nbsp;&nbsp;</span>padding-right: 10px;</div>
+              </div>
+              <div>
+                <span>&nbsp;&nbsp;</span><span v-if="product.paddingY">{{product.paddingY}}</span><span v-else>padding-top: 10px</span>;
+              </div>
+            </div>
+            <div><span>&nbsp;&nbsp;</span>transition: 0.3s;</div>
+            <div class="reactive">
+              <span>&nbsp;&nbsp;</span>background-color: <span v-if="product.background">{{product.background}}</span><span v-else>#ffffff</span>;
+            </div>
+            <div class="reactive">
+              <span>&nbsp;&nbsp;</span>color: <span v-if="product.color">{{product.color}}</span><span v-else>#000000</span>;
+            </div>
+          }
+        </div>
+
+        <div class="class-block">
+          <div>
+            .cm-product-title {
+
+            }
+          </div>
+        </div>
+
+
+
 
       </code>
     </div>
@@ -20,11 +86,21 @@ export default {
   data: function() {
     return {
       export: "",
-      css: []
+      css: {
+        product: [],
+        title: [],
+        price: [],
+        sale: [],
+        saleEmblem: [],
+        image: [],
+        addToCart: [],
+        viewProduct: []
+      }
     }
   },
   computed: {
     ...mapState({
+      cell: state => state.customiser.cOptions.cell,
       product: state => state.customiser.cOptions.product,
       image: state => state.customiser.cOptions.image,
       title: state => state.customiser.cOptions.title,
@@ -34,5 +110,15 @@ export default {
       viewProduct: state => state.customiser.cOptions.viewProduct
     })
   },
+  mounted: function(){
+    this.generateCSS();
+  },
+  methods: {
+    generateCSS(){
+      for (let i = 0; i < this.product.length; i++){
+        console.log(this.product[i]);
+      }
+    }
+  }
 }
 </script>
